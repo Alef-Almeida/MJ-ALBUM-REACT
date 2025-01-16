fb7b994d1d1184d089a396ca1c73be39519efb44
Alefhkk <95300761+Alef-Almeida@users.noreply.github.com>
2025-01-16 16:55:15 -0300

    Navbar

---
+++ albumreact/thiller/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <Link to="/" className="logo">logo</Link>
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;  
---
+++ albumreact/thiller/src/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './login.css'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>TESTE LOGIN</h1>
+    </div>
+  )
+}
+
+export default Login
